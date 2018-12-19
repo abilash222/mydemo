cf19aadcde0f138b6dfee91301b7ee513d4ecfb8
abilash <abilash_222@yahoo.co.in>
2018-12-18 18:54:54 -0800

    redis port

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ logging.basicConfig(stream=sys.stdout)
 DEBUG = True
 
 REDIS_HOST = '127.0.0.1'
-REDIS_PORT = 1234
+REDIS_PORT = 6379
 REDIS_DB = 0
 
 TEXT_API = 'http://www.randomtext.me/api/gibberish'
